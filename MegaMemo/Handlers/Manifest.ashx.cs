294a950702430429242fae8c32ffd76f6bb0505a
emiljas <emiljasinski@gmail.com>
2013-10-20 23:15:23 +0200

    Manifest.txt

---
+++ MegaMemo/Handlers/Manifest.ashx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaMemo.Controllers
+{
+    public class Manifest : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/cache-manifest";
+            context.Response.WriteFile(context.Server.MapPath("~/Content/Manifest.txt"));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}

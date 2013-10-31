073cecc65b2726520f0ff678ebfc6050bc5bdbec
emiljas <emiljasinski@gmail.com>
2013-10-31 22:55:15 +0100

    działa rejestracja bez twittera i micrososftu

--- MegaMemo/Global.asax.cs
+++ MegaMemo/Global.asax.cs
@@ -1,14 +1,18 @@
-﻿using MegaMemo.Models;
+﻿using MegaMemo.Migrations;
+using MegaMemo.Models;
 using MegaMemo.Models.Context;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
+using WebMatrix.WebData;
 
 namespace MegaMemo
 {
@@ -17,9 +21,13 @@ namespace MegaMemo
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static SimpleMembershipInitializer _initializer;
+        private static object _initializerLock = new object();
+        private static bool _isInitialized;
+
         protected void Application_Start()
         {
-            Database.SetInitializer(new DataInitializer());
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MegaMemoDbContext, Configuration>());
             MegaMemoDbContext db = new MegaMemoDbContext();
             db.Database.Initialize(true);
 
@@ -30,6 +38,8 @@ namespace MegaMemo
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
         }
     }
 }
